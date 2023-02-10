f508b1e47b6953fce59dba74601715a683c7bf2b
Deepak Yadav <dy22900@gmail.com>
2023-02-10 20:20:18 +0530

    optimized

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,121 +1,92 @@
-// ignore_for_file: file_names
-
-import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import "package:flutter/material.dart";
+import "package:font_awesome_flutter/font_awesome_flutter.dart";
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.white,
       child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 39.0),
+        padding: const EdgeInsets.only(top: 40),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Padding(
-              padding: EdgeInsets.only(top: 30),
-              child: Icon(
-                FontAwesomeIcons.twitter,
-                color: Color.fromARGB(255, 29, 161, 242),
-              ),
+            const Icon(
+              FontAwesomeIcons.twitter,
+              size: 30,
+              color: Color.fromARGB(255, 29, 161, 242),
             ),
-            const SizedBox(
-              height: 200,
-            ),
-            const Text(
-              "See What's happening ",
-              style: TextStyle(
-                fontSize: 30,
-                fontFamily: "twitterchirp.ttf",
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 20, 23, 26),
-              ),
-            ),
-            const Text(
-              "in the world right now.",
-              style: TextStyle(
-                fontSize: 30,
-                fontFamily: "twitterchirp.ttf",
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 20, 23, 26),
-              ),
-            ),
-            const SizedBox(
-              height: 180,
-            ),
-            AnimatedContainer(
-              decoration: BoxDecoration(
-                border:
-                    Border.all(color: const Color.fromARGB(255, 170, 184, 194)),
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.white,
-              ),
-              height: 45,
-              width: 300,
-              duration: const Duration(seconds: 1),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    "assets/images/google-logo-9808.png",
-                    height: 28,
-                    width: 30,
-                    alignment: Alignment.center,
-                  ),
-                  const SizedBox(
-                    width: 7,
-                  ),
-                  const Text(
-                    "Continue with Google",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            AnimatedContainer(
-              duration: const Duration(seconds: 1),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.black,
-              ),
-              height: 45,
-              width: 300,
-              alignment: Alignment.center,
-              child: const Text(
-                "Create account",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: "twitterchirp.ttf"),
-              ),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Row(
-              children: [
-                const Text(
-                  "By signing up, you agree to our",
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 101, 119, 134),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  style: const ButtonStyle(),
-                  child: const Text("Terms"),
-                ),
-              ],
-            )
+            const SizedBox(height: 200),
+            _buildText("See What's Happening", 30, "twitterchirp.ttf"),
+            _buildText("in the world right now.", 30, "twitterchirp.ttf"),
+            const SizedBox(height: 180),
+            _buildGoogleLoginButton(),
+            const SizedBox(height: 20),
+            _buildCreateAccountButton(),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildText(String text, double fontSize, String fontFamily) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: fontSize,
+        fontFamily: fontFamily,
+        fontWeight: FontWeight.bold,
+        color: const Color.fromARGB(255, 20, 23, 26),
+      ),
+    );
+  }
+
+  Widget _buildGoogleLoginButton() {
+    return AnimatedContainer(
+      duration: const Duration(seconds: 1),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: const Color.fromARGB(255, 170, 184, 194)),
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+      ),
+      height: 45,
+      width: 300,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/images/google-logo-9808.png",
+            height: 28,
+            width: 28,
+            alignment: Alignment.center,
+          ),
+          const SizedBox(width: 7),
+          const Text(
+            "Continue With Google",
+            style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCreateAccountButton() {
+    return AnimatedContainer(
+      duration: const Duration(seconds: 1),
+      decoration: const BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+      height: 45,
+      width: 300,
+      alignment: Alignment.center,
+      child: const Text(
+        "Create account",
+        style: TextStyle(
+            fontSize: 14, fontWeight: FontWeight.bold, color: Colors.white),
       ),
     );
   }
